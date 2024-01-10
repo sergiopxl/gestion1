4e87d9911df9e6a258bce603d6d440e820a5e213
Mario Guerra <mgg7007@gmail.com>
2024-01-10 19:20:49 +0100

    Conexión a base de datos

---
+++ api/conn/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+include("conn/parameters.php");
+
+$conn = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die("No se puede conectar a la base de datos");
+mysqli_select_db($conn, DATABASE);
+mysqli_set_charset($conn,"UTF8");
+
+if ($conn->connect_error) {
+    die("La conexión ha fallado: " . $conn->connect_error);
+} else {
+    // echo "<div>La conexión a la base de datos ha tenido éxito</div>";
+}
+
+?>
---
+++ api/conn/parameters.php
@@ -0,0 +1,11 @@
+<?php
+
+define("HOST","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DATABASE","gestion_empresarial");
+
+define("CAN_REGISTER","any");
+define("DEFAULT_ROLE","member");
+
+?>
