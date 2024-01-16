8c61d1e1507a483d1043ae77027c0a4f1e96f054
Mario Guerra <mgg7007@gmail.com>
2024-01-16 16:00:16 +0100

    Renombra la base de datos a 'gestion'

--- api/conn/parameters.php
+++ api/conn/parameters.php
@@ -3,7 +3,7 @@
 define("HOST","localhost");
 define("USER","root");
 define("PASSWORD","");
-define("DATABASE","gestion_empresarial");
+define("DATABASE","gestion");
 
 define("CAN_REGISTER","any");
 define("DEFAULT_ROLE","member");
