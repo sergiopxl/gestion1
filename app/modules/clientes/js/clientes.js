aad8c3152cc80d18b6d0ef1be9db615bc5f7593f
Mario Guerra <mgg7007@gmail.com>
2024-01-16 19:11:37 +0100

    Clientes: Muestra los contactos de cada cliente

--- app/modules/clientes/js/clientes.js
+++ app/modules/clientes/js/clientes.js
@@ -6,6 +6,7 @@ function doClientes() {
 
     const contenedorListado = document.querySelector("main")
     const templateCliente = document.querySelector(".cliente-row")
+    const templateContacto = document.querySelector(".contactos-contacto")
 
     function getClientes() {
 
@@ -17,15 +18,40 @@ function doClientes() {
     }
 
     function printListaClientes(clientes) {
+
+        contenedorListado.innerHTML = ""
+
         clientes.forEach(cliente => {
             const clienteContenedor = templateCliente.cloneNode(true)
+
+            clienteContenedor.classList.remove("hidden")
 
             clienteContenedor.querySelector(".cliente-datos-nombre").textContent = cliente.nombre;
             clienteContenedor.querySelector(".cliente-datos-cif").textContent = cliente.cif;
             clienteContenedor.querySelector(".cliente-datos-tlf").textContent = cliente.telefono;
             clienteContenedor.querySelector(".cliente-datos-direccion").textContent = cliente.direccion;
+            clienteContenedor.querySelector(".cliente-datos-sector").textContent = "";
+
+            printListaContactos(clienteContenedor, cliente.contactos)
 
             contenedorListado.append(clienteContenedor)
+        });
+    }
+
+    function printListaContactos(cliente, contactos) {
+
+        const contenedorContactos = cliente.querySelector(".cliente-row-contactos")
+
+        contenedorContactos.innerHTML = ""
+
+        contactos.forEach(contacto => {
+            const contactoContenedor = templateContacto.cloneNode(true)
+
+            contactoContenedor.querySelector(".contacto-nombre").textContent = `${contacto.nombre} ${contacto.apellido1} ${contacto.apellido2}`
+            contactoContenedor.querySelector(".contacto-tlf").textContent = contacto.telefono1
+            contactoContenedor.querySelector(".contacto-email").textContent = contacto.email1
+
+            contenedorContactos.append(contactoContenedor)
         });
     }
 
