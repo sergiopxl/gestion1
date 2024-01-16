a400d9f2af25ece6754dbdc1ca8338a8c39327d4
Mario Guerra <mgg7007@gmail.com>
2024-01-16 21:45:16 +0100

    Clientes: Implementación de búsqueda en front-end

--- app/modules/clientes/js/clientes.js
+++ app/modules/clientes/js/clientes.js
@@ -11,32 +11,72 @@ function doClientes() {
     const templateCliente = document.querySelector(".cliente-row")
     const templateContacto = document.querySelector(".contactos-contacto")
 
-    function getClientes(actual) {
+    // Prepara el buscador de clientes
+    const buscadorTexto = document.querySelector("#buscador-input")
+    const buscadorBoton = document.querySelector("#buscador-boton")
 
-        if (actual)
-            paginaActual = actual
+    buscadorBoton.addEventListener("click", () => {
+        const busqueda = buscadorTexto.value
+        if (busqueda != "") {
+            getClientes(paginaActual = 1, busqueda)
+        }
+    })
+
+    // Función de carga de los clientes de una página en concreto y con una
+    // expresión de búsqueda.
+    function getClientes(pagina, busqueda) {
+
+        // Paginación
+        if (pagina)
+            paginaActual = pagina
 
         const inicio = paginaActual > 1
             ? (paginaActual - 1) * resultadosPorPagina
             : 0
         const parametroInicio = `?inicio=${inicio}`
-        const parametroPorPagina = `&porpagina=${resultadosPorPagina}`
+        let parametroPorPagina = `&porpagina=${resultadosPorPagina}`
 
-        const url = apiUrlClientesGet + parametroInicio + parametroPorPagina
+        // Búsqueda
+        let busquedaActiva = false
+        let parametroBusqueda = ""
+        if (busqueda && busqueda != "") {
+            parametroBusqueda = `&buscar=${busqueda}`
+            parametroPorPagina = "&porpagina=99999"
+            busquedaActiva = true
+        }
+
+        // Hace la llamada GET al API e imprime los resultados
+        const url = apiUrlClientesGet + parametroInicio + parametroPorPagina + parametroBusqueda
 
         fetch(url, { method: "GET" }).then(respuesta =>
             respuesta.json().then(clientes => {
-                printListaClientes(clientes.numero_clientes, clientes.clientes)
+                printListaClientes(clientes.numero_clientes, clientes.clientes, busquedaActiva)
             })
         )
     }
 
-    function printListaClientes(numClientes, clientes) {
+    // Lista los clientes en la interfaz
+    function printListaClientes(numClientes, clientes, busquedaActiva) {
 
         contenedorListado.innerHTML = ""
 
         // Preparamos la paginación
-        doPaginacion(paginaActual, resultadosPorPagina, numClientes, getClientes)
+        if (!busquedaActiva) {
+            buscadorTexto.value = ""
+            document.querySelector("#paginacion").innerHTML = "<ul></ul>"
+
+            doPaginacion(paginaActual, resultadosPorPagina, numClientes, getClientes)
+        } else {
+            const botonVerTodos = document.createElement("button")
+            botonVerTodos.classList.add("btn-info")
+            botonVerTodos.textContent = "Ver listado completo"
+            botonVerTodos.addEventListener("click", () => getClientes())
+
+            document.querySelector("#paginacion").innerHTML = numClientes == 1
+                ? "<h2>1 cliente encontrado</h2>"
+                : `<h2>${numClientes} clientes encontrado</h2>`
+            document.querySelector("#paginacion").append(botonVerTodos)
+        }
 
         // Imprime los clientes devueltos
         clientes.forEach(cliente => {
@@ -56,6 +96,7 @@ function doClientes() {
         });
     }
 
+    // Lista los contactos de un cliente en la interfaz
     function printListaContactos(cliente, contactos) {
 
         const contenedorContactos = cliente.querySelector(".cliente-row-contactos")
