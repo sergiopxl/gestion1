8a97ec44234d249c3d2ebd823a929d75a355b825
Mario Guerra <mgg7007@gmail.com>
2024-01-23 14:39:52 +0100

    Clientes: API para eliminar Contactos

---
+++ api/clientes_contactos_delete.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID, X-Token, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: GET");
+
+include("conn/conexion.php");
+
+$idContacto = $_GET["contacto-id"];
+
+$sqlContactosUpdate = "DELETE FROM clientes_contactos_tb
+                       WHERE id = $idContacto";
+
+$respuesta = mysqli_query($conn, $sqlContactosUpdate);
+
+$mensaje = $respuesta ? "Registro eliminado correctamente" : "Tienes un problema";
+echo json_encode($mensaje);
