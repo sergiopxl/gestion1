42cfd65787a1534ef51a7e7c9737f5c83bd5e345
Mario Guerra <mgg7007@gmail.com>
2024-01-31 17:04:54 +0100

    Informes: API para obtener clientes que más facturan

--- api/facturas_get.php
+++ api/facturas_get.php
@@ -14,39 +14,73 @@ $error = false;
 // Cálculo de estadísticas de facturación
 if (isset($_GET["estadisticas"])) {
 
-    // Consulta el total facturado y las fechas de inicio y fin
-    $sqlTotalFacturado = "SELECT SUM(baseimponible * (1 + iva / 100)) AS total_facturas,
-                                 MIN(CAST(fecha_emision AS DATE)) AS fecha_inicio,
-                                 MAX(CAST(fecha_emision AS DATE)) AS fecha_fin
-                          FROM facturas_tb";
+    if ($_GET["estadisticas"] == "cliente") {
+        
+        // Estadísticas de facturas por cliente
 
-    $respuestaTotalFacturado = mysqli_query($conn, $sqlTotalFacturado);
+        $sqlMejoresClientes = "SELECT facturas_tb.id_cliente, clientes_tb.nombre, SUM(facturas_tb.baseimponible) AS total_facturado
+                                 FROM facturas_tb
+                                 JOIN clientes_tb ON facturas_tb.id_cliente = clientes_tb.id
+                               WHERE facturas_tb.id_estado = 3
+                               GROUP BY facturas_tb.id_cliente
+                               ORDER BY total_facturado DESC";
 
-    if ($respuestaTotalFacturado) {
-        $fila = mysqli_fetch_assoc($respuestaTotalFacturado);
-        $respuesta = $fila;
-    }
-    else $error = true;
+        // Si se ha especificado un máximo de resultados, añadimos el límite
+        if (isset($_GET["mostrar"])) {
 
-    // Consulta el subtotal facturado por fecha
-    $sqlFacturasPorFecha = "SELECT SUM(baseimponible * (1 + iva / 100)) AS facturado, 
-                                   fecha_emision AS fecha, 
-                                   COUNT(*) as num_facturas
-                            FROM facturas_tb
-                            GROUP BY fecha";
-    
-    $respuestaFacturasPorFecha = mysqli_query($conn, $sqlFacturasPorFecha);
-
-    if ($respuestaFacturasPorFecha) {
-
-        $facturasPorFecha = [];
-        while ($fila = mysqli_fetch_assoc($respuestaFacturasPorFecha)) {
-            $facturasPorFecha[] = $fila;
+            $limite = (int) $_GET["mostrar"];
+            if ($limite > 0)
+                $sqlMejoresClientes .= " LIMIT $limite";
         }
+        
 
-        $respuesta["facturas_por_fecha"] = $facturasPorFecha;
+        $respuestaMejoresClientes = mysqli_query($conn, $sqlMejoresClientes);
+            
+        if ($respuestaMejoresClientes) {
+            while ($fila = mysqli_fetch_assoc($respuestaMejoresClientes)) {
+                $respuesta[] = $fila;
+            }
+        }
+        else $error = true;
     }
-    else $error = true;
+    else {
+
+        // Estadísticas generales de facturación total y por fecha
+
+        // Consulta el total facturado y las fechas de inicio y fin
+        $sqlTotalFacturado = "SELECT SUM(baseimponible * (1 + iva / 100)) AS total_facturas,
+                                     MIN(CAST(fecha_emision AS DATE)) AS fecha_inicio,
+                                     MAX(CAST(fecha_emision AS DATE)) AS fecha_fin
+                              FROM facturas_tb";
+    
+        $respuestaTotalFacturado = mysqli_query($conn, $sqlTotalFacturado);
+    
+        if ($respuestaTotalFacturado) {
+            $fila = mysqli_fetch_assoc($respuestaTotalFacturado);
+            $respuesta = $fila;
+        }
+        else $error = true;
+    
+        // Consulta el subtotal facturado por fecha
+        $sqlFacturasPorFecha = "SELECT SUM(baseimponible * (1 + iva / 100)) AS facturado, 
+                                       fecha_emision AS fecha, 
+                                       COUNT(*) as num_facturas
+                                FROM facturas_tb
+                                GROUP BY fecha";
+        
+        $respuestaFacturasPorFecha = mysqli_query($conn, $sqlFacturasPorFecha);
+    
+        if ($respuestaFacturasPorFecha) {
+    
+            $facturasPorFecha = [];
+            while ($fila = mysqli_fetch_assoc($respuestaFacturasPorFecha)) {
+                $facturasPorFecha[] = $fila;
+            }
+    
+            $respuesta["facturas_por_fecha"] = $facturasPorFecha;
+        }
+        else $error = true;
+    }
 }
 
 if (!$error)
